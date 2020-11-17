0be7924466096ee0cadb6eb67389b630b068fab6
Ife5983 <modulehelen44@gmail.com>
2020-11-17 11:26:02 -0800

    Initial commit

---
+++ src/main/java/com/petstore/petstore/web/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.petstore.petstore.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/welcome")
+    public @ResponseBody String welcomeMessage(){
+        return "Pet Store Application Running!";
+    }
+    @GetMapping("/")
+    public String displayWelcomePage(){
+        return "welcome";
+    }
+}
